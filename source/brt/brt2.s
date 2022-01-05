c23da2db4ba746944fbcfef57e0fcad1e74ab526
phil <phil@ultimate.com>
2022-01-04 20:13:14 -0500

    brt2.s: stack comment

--- source/brt/brt2.s
+++ source/brt/brt2.s
@@ -3,6 +3,8 @@
 .bss
 .globl stke
 
+// maybe put B stack below "sp" stack
+// (ie; subtract some number from r6???)
 stksiz = 512.	/ stack size for B stack (r5)
 
 stkb: .=.+stksiz / stack, (r5) points into this
